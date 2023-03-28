a2367371607f83e6cca5a6e907ee13efca54859b
Romuald-Duret <romuald.duret.etu@gmail.com>
2023-03-28 14:24:39 +0200

    Séparation de la méthode de test dans un autre fichier.

---
+++ testSolution.hpp
@@ -0,0 +1,39 @@
+//
+//  testSolution.hpp
+//  CPLEX_Test
+//
+//  Created by Romuald Duret on 28/03/2023.
+//
+
+
+
+
+#ifndef testSolution_hpp
+#define testSolution_hp
+
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include<string.h>
+#include "json.hpp"
+#include <list>
+#include <tuple>
+#include <iterator>
+#include <ilcplex/ilocplex.h>
+#include "Data.hpp"
+
+ILOSTLBEGIN
+
+/*
+ Racourcis
+*/
+using namespace std;
+// orderered_json sinon il va trier par ordre alphabetique
+using json = nlohmann::ordered_json;
+
+void testSolution(string filename, Data * mydata);
+
+
+#endif /* testSolution_hpp */
+
